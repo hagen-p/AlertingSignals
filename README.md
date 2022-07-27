aebd0e4b08b27e386e930d7d38909ba2200331b9
p-hagen-sfx <phagen@signalfx.com>
2022-07-27 16:13:31 +0200

    Outlier Detection completed

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # README
 
 Check out this repository and change into the directory `AlertingSignals`
-Create a config.yaml in the root directory that contains the following  where xx is a valid ingest Access token for Splunk observability and yyy is the realm you  are using ie eu0,us0,us1, jp0:
+Create a config.yaml in the root directory that contains the following  where:
+- xxx is a valid ingest Access token for Splunk observability
+- yyy is the value of the realm you are using ie *eu0,us0,us1*
 
-```text
+
+```yaml
 
 ACCESS_TOKEN: xxx
 REALM: yyy
+OD_ALERT: False
 ```
 
 Set up your env by running the following to install the python dependencies and run the main app 
@@ -18,3 +22,6 @@ pip install -r requirements.txt
 python3 main.py
 ```
 
+To test an Outlier Detection, Run the OD signal from the main.py app.
+
+To Trigger the condition let  the app run for at least 10 minutes then change the value of OD_ALERT in the config.yal to True, and keep it set for at least one minute  ( you can set the alert  for 80% of 1 Minute)
--- Signals/server.py
+++ Signals/server.py
@@ -1,59 +1,79 @@
-import socket
+import os
+from signal import Signals
 import yaml
 import time
+import random
 import Signals.splunk as metrics
 
 
 with open("config.yaml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
-
 def start_Signal(signaltype):
+    random.seed()
+    ODseed = 15
+    ODMinute = 0
+    ODcheck = False 
     print(f"Starting signalType {signaltype} ...")
- 
-    # Receive Packages
-    setHigh = True
-    while True:   
-        time.sleep(10)
-        if setHigh == True:
+    while True:
+        try:
+            f=open ("Alerts/OD.alert","r")
+            org_path=os.path.realpath(f.name)
+            dest_path = os.path.join(os.path.dirname(os.path.realpath(f.name)),'OD.old.alert')
+            ODcheck = True
+        except FileNotFoundError:
+            ODcheck = False
+
+        if  ODcheck == False:
             metrics.write_outlier_data_to_splunk(
-                "Linux-1",80, 1024 )
+                "Linux-1",80 + random.randrange(ODseed)  , 1024 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-2",99, 2024 )
+                "Linux-2",80 + random.randrange(ODseed), 2024 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-3",78, 1144 )
+                "Linux-3",80 + random.randrange(ODseed), 1144 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-4",82, 1824 )
+                "Linux-4",80 + random.randrange(ODseed), 1824 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-5",89, 1424 )
+                "Linux-5",80 + random.randrange(ODseed), 1424 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-6",87, 1224 )
+                "Linux-6",80 + random.randrange(ODseed), 1224 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-7",91, 3024 )
+                "Linux-7",80 + random.randrange(ODseed), 3024 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-8",75, 1324 )
+                "Linux-8",80 + random.randrange(ODseed), 1324 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-9",66, 454 )
+                "Linux-9",80 + random.randrange(ODseed), 454 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-0",69, 728 )
+                "Linux-0",80 + random.randrange(ODseed), 728 )
             #setHigh = False    
-        else:        
+        elif ODcheck ==  True: 
+            print (60-ODMinute)       
             metrics.write_outlier_data_to_splunk(
-                "Linux-1", 60, 400)
+                "Linux-1",80 + random.randrange(ODseed)  , 1024 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-2",55, 1024 )
+                "Linux-2",80 + random.randrange(ODseed), 2024 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-3",78, 644 )
+                "Linux-3",80 + random.randrange(ODseed), 1144 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-5",89, 824 )
+                "Linux-4",80 + random.randrange(ODseed), 1824 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-6",87, 1024 )
+                "Linux-5",80 + random.randrange(ODseed), 1424 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-7",80, 2046 )
+                "Linux-6",80 + random.randrange(ODseed), 1224 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-8",34, 624 )
+                "Linux-7",80 + random.randrange(ODseed), 3024 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-9",44, 354 )
+                "Linux-8",80 + random.randrange(ODseed), 1324 )
             metrics.write_outlier_data_to_splunk(
-                "Linux-0",69, 728 )
-            setHigh = True
+                "Linux-9",40 + random.randrange(ODseed), 454 )
+            metrics.write_outlier_data_to_splunk(
+                "Linux-0",80 + random.randrange(ODseed), 728 )
+            ODMinute = ODMinute + 10 # Add 10 seconds to counter
+            if ODMinute >= 60: # more then a minute has gone by    
+                os.rename(org_path, dest_path) #renaming the alert file to stop the Outlier
+                ODMinute = 0 #reset
+        else:
+            print ("OD_ALERT needs to be set to True of False")
+        
+        time.sleep(10) # wait 10 second
+       
